5a5a09ef449296ed71a80fa2924c729d3d8b88fd
111q-33 <2454924181@qq.com>
2021-02-07 23:09:17 +0800

    null

---
+++ practice_2_7/practice_2_7/test.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int a[5] = { 1, 2, 3, 4, 5 };
+//	int *ptr = (int *)(&a + 1);
+//	printf("%d,%d", *(a + 1), *(ptr - 1));// 2 5
+//	return 0;
+//}//由于还没学习结构体，这里告知结构体的大小是20个字节
+//struct Test
+//{
+//	int Num;
+//	char *pcName;
+//	short sDate;
+//	char cha[2];
+//	short sBa[4];
+//}*p;
+////假设p 的值为0x100000。 如下表表达式的值分别为多少？
+//int main()
+//{
+//	printf("%p\n", p + 0x1);//0x100014
+//	printf("%p\n", (unsigned long)p + 0x1);//0x100001
+//	printf("%p\n", (unsigned int*)p + 0x1);//0x100004
+//	return 0;
+//}
+//int main()
+//{
+//	int a[4] = { 1, 2, 3, 4 };
+//	int *ptr1 = (int *)(&a + 1);
+//	int *ptr2 = (int *)((int)a + 1);
+//	printf("%x,%x", ptr1[-1], *ptr2);// 4 2000000
+//	return 0;
+//}
+
+
+int main()
+{
+	int a[3][2] = { (0, 1), (2, 3), (4, 5) };
+	int *p;
+	p = a[0];
+	printf("%d", p[0]);//1
+	return 0;
+}
